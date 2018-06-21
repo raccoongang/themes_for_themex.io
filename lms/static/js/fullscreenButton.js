98a83801a1cea10bbec631d0b18e652164e05d58
Misha <suxx_2000@hotmail.com>
2018-06-21 16:25:22 +0300

    conflict fixed (#177)

--- lms/static/js/fullscreenButton.js
+++ lms/static/js/fullscreenButton.js
@@ -1,28 +1,42 @@
+'use strict';
+
 $(function () {
-    let fs = document.getElementById('fullscreen');
-    let main = document.getElementById('course-content');
+    var fs = document.getElementById('fullscreen');
+    var main = document.getElementById('course-content');
 
     function toggleFullScreen() {
-        let fullscreen = document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
+        var fullscreen = document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
         console.log(fs);
         if (!fullscreen) {
-            if (main.msRequestFullscreen) {main.msRequestFullscreen()}
-            if (main.webkitRequestFullscreen) {main.webkitRequestFullscreen()}
-            if (main.mozRequestFullScreen) {main.mozRequestFullScreen()}
+            if (main.msRequestFullscreen) {
+                main.msRequestFullscreen();
+            }
+            if (main.webkitRequestFullscreen) {
+                main.webkitRequestFullscreen();
+            }
+            if (main.mozRequestFullScreen) {
+                main.mozRequestFullScreen();
+            }
             cahngeXlink('#ico-fs-shrink');
         } else {
-            if(document.msExitFullscreen) {document.msExitFullscreen()}
-            if(document.mozCancelFullScreen) {document.mozCancelFullScreen()}
-            if(document.webkitExitFullscreen) {document.webkitExitFullscreen()}
+            if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+            if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            }
+            if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
             cahngeXlink('#ico-fs-expand');
         }
     }
 
-    let cahngeXlink = (val) => {
+    var cahngeXlink = function cahngeXlink(val) {
         document.querySelector('use').setAttributeNS('http://www.w3.org/1999/xlink', 'href', val);
     };
 
-    fs.addEventListener('click', () => {
+    fs.addEventListener('click', function () {
         toggleFullScreen();
-    })
-});
+    });
+})
