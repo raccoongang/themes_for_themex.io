c849d37d19941746948c57f0ae1c8bf7d0527862
ihor-romaniuk <Igorek25>
2019-08-28 22:08:24 +0300

    fix

--- lms/static/js/slider.js
+++ lms/static/js/slider.js
@@ -5,8 +5,43 @@ $(window).load(function () {
         speed: 700,
         fade: true,
         cssEase: 'linear',
+    });
+
+    $('.testimonials-list').slick({
+        infinite: true,
+        speed: 700,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        centerPadding: '30px',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    $('.partners-list').slick({
+        infinite: true,
+        speed: 700,
         autoplay: true,
-        autoplaySpeed: 7000
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        variableWidth: true,
+        centerPadding: '30px',
     });
 });
 
